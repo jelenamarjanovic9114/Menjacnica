9ca5d6bf021095cb0d8e70fe71aeb667cb84ff22
jelenamarjanovic9114 <jmarjanovic@hotmail.com>
2016-03-19 19:00:03 +0100

    Implementirane metode interfejsa u okviru master grane.

--- Menjacnica/src/menjacnica/MenjacnicaImplementacija.java
+++ Menjacnica/src/menjacnica/MenjacnicaImplementacija.java
@@ -8,19 +8,26 @@ public class MenjacnicaImplementacija implements MenjacnicaInterfejs {
 
 	@Override
 	public void dodajNoviKurs(Valuta valuta, Kurs kurs) {
-		
-
+		valuta.getKursevi().add(kurs);
 	}
 
 	@Override
 	public void obrisiKursValute(Valuta valuta, Kurs kurs, GregorianCalendar datum) {
-		
-
+		valuta.getKursevi().remove(kurs);
 	}
 
 	@Override
 	public Kurs vratiKursValute(Valuta valuta, GregorianCalendar datum) {
-		
+		for(int i = 0; i < valuta.getKursevi().size(); i++){
+			Kurs k = valuta.getKursevi().get(i);
+			GregorianCalendar datumValute = k.getDatumKursa();
+			if(datum.get(GregorianCalendar.DAY_OF_MONTH) == datumValute.get(GregorianCalendar.DAY_OF_MONTH) &&
+					datum.get(GregorianCalendar.MONTH) == datumValute.get(GregorianCalendar.MONTH) &&
+					datum.get(GregorianCalendar.YEAR) == datumValute.get(GregorianCalendar.YEAR)){
+				return k;
+			}
+		}
+		System.out.println("Kurs za taj datum ne postoji! Vraceno NULL.");
 		return null;
 	}
 
