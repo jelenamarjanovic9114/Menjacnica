ac7a69b60ec656c178440fed3817cfa3680964c1
jelenamarjanovic9114 <jmarjanovic@hotmail.com>
2016-03-19 18:09:07 +0100

    Napravljena klasa koja implementira MenjacnicaInterfejs, prazne metode

---
+++ Menjacnica/src/menjacnica/MenjacnicaImplementacija.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class MenjacnicaImplementacija implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajNoviKurs(Valuta valuta, Kurs kurs) {
+		
+
+	}
+
+	@Override
+	public void obrisiKursValute(Valuta valuta, Kurs kurs, GregorianCalendar datum) {
+		
+
+	}
+
+	@Override
+	public Kurs vratiKursValute(Valuta valuta, GregorianCalendar datum) {
+		
+		return null;
+	}
+
+}
